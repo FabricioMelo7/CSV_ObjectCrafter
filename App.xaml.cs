1fb7df3fd7eaaad234cbe722fa0bbd2ecc6a6657
Fabricio Melo <fabriciosartin4@hotmail.com>
2024-09-13 17:48:41 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSV_ObjectCrafter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
